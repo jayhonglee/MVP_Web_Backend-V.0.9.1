1ddb6a703c5ff2bfa5de44a0ad44a47680b3e59e
Jaehong Lee <ja34luv@gmail.com>
2025-10-21 21:36:29 -0700

    chore: add console output for debugging in production

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -23,11 +23,15 @@ const auth = async (req, res, next) => {
       throw new Error("No token provided");
     }
 
+    console.log("token:", token);
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("decoded:", decoded);
     const user = await User.findOne({
       _id: decoded._id,
       "tokens.token": token,
     });
+    console.log("user:", user);
 
     if (!user) {
       throw new Error("User not found");
